7f57b2cf7354681bb21505328ad83a4864a6b15b
Ayodeji Bankole <bankymono@gmail.com>
2025-02-05 09:43:26 +0100

    #

---
+++ src/array_problems/Problem10.java
@@ -0,0 +1,40 @@
+package array_problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        String s = "eceba";
+        int k = 2;
+
+        System.out.println(substringWithMostKChar(s, k));
+    }
+
+
+    public static int substringWithMostKChar(String s, int k) {
+        Map<Character, Integer> charMap = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int left = 0;
+        int count = 0;
+        for(int right = 0; right < s.length(); ++right) {
+            char currentChar = s.charAt(right);
+
+            charMap.put(currentChar, charMap.getOrDefault(currentChar, 0) + 1);
+
+
+            while(charMap.size() > k) {
+                char leftChar = s.charAt(left);
+                charMap.put(leftChar, charMap.get(leftChar) - 1);
+                if(charMap.get(leftChar) == 0) {
+                    charMap.remove(leftChar);
+                }
+                left++;
+            }
+
+            max= Math.max(right - left + 1, max);
+        }
+
+        return max;
+    }
+}
