57eeb798817584dcaae84c25702cefb875cfab1c
Ayodeji Bankole <bankymono@gmail.com>
2025-02-15 21:17:47 +0100

    #

--- src/sliding_window/Problem17.java
+++ src/sliding_window/Problem17.java
@@ -2,33 +2,33 @@ package sliding_window;
 
 public class Problem17 {
     public static void main(String[] args) {
-        int[] nums = {1,1,0,1,1,1,1,0,1,1};
+        int[] nums = {1,0,1,1,0,1,1,1};
 
         System.out.println(longestOneArrays(nums));
 
     }
 
     public static int longestOneArrays(int[] nums) {
-        int left = 0;
+        int left = 0, maxLength = 0;
         int zeroCount = 0;
-        int maxLength = 0;
 
-        for(int right = 0; right < nums.length; right++) {
-            int currentNum = nums[right];
-
-            if(currentNum == 0){
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
                 zeroCount++;
             }
 
-            if(zeroCount > 1) {
-                left = right + 1;
-                zeroCount--;
+            // If there are more than 1 zero, shrink the window from the left
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
             }
 
-
-            maxLength = Math.max(maxLength, right - left + 1);
+            // We must delete **exactly one** element, so length is (right - left)
+            maxLength = Math.max(maxLength, right - left);
         }
 
-        return maxLength - 1;
+        return maxLength;
     }
 }
