605f8f4f10dfd6e9b28d9172500863ea65f2bf75
Ayodeji Bankole <bankymono@gmail.com>
2025-02-20 19:25:02 +0100

    #

--- src/dynamic_programming/Fibonacci.java
+++ src/dynamic_programming/Fibonacci.java
@@ -1,6 +1,11 @@
 package dynamic_programming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
+    private static Map<Integer, Long> memo = new HashMap<>();
+
     public static void main(String[] args) {
         System.out.println(fib(6));
         System.out.println(fib(7));
@@ -9,7 +14,14 @@ public class Fibonacci {
     }
 
     public static long fib(int n) {
+        if(memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
         if(n <= 2) return 1;
-        return fib(n - 1) + fib(n - 2);
+        long ans = fib(n - 1) + fib(n - 2);
+        memo.put(n, ans);
+
+        return ans;
     }
 }
