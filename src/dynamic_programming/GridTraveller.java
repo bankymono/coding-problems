cb827bc8b11c878cdcf343b5cd008889097a2bf8
Ayodeji Bankole <bankymono@gmail.com>
2025-02-22 21:02:56 +0100

    #

--- src/dynamic_programming/GridTraveller.java
+++ src/dynamic_programming/GridTraveller.java
@@ -4,24 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GridTraveller {
-    private static final Map<String, Integer> memo = new HashMap<>();
+    private static final Map<String, Long> memo = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println(gridTraveller(2,3));
+        System.out.println(gridTraveller(18,18));
     }
 
-    public static int gridTraveller(int m, int n) {
+    public static long gridTraveller(int m, int n) {
+        String key = m + "," + n;
+        if(memo.containsKey(key)) return memo.get(key);
+
         if(m == 0) return 0;
         if(n == 0) return 0;
         if(m == 1) return 1;
         if(n == 1) return 1;
-        String key = m + "," + n;
+
         
-        if(memo.containsKey(key)) return memo.get(key);
 
 
 
-        int result = gridTraveller(m, n - 1) + gridTraveller(m - 1, m);
+
+        long result = gridTraveller(m, n - 1) + gridTraveller(m - 1, n);
         memo.put(key, result);
         return result;
     }
