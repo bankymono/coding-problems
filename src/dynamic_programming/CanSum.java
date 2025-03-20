9fc50d2ac67cf423010949e7991f1b6bda90119c
Ayodeji Bankole <bankymono@gmail.com>
2025-03-20 18:02:27 +0100

    #

--- src/dynamic_programming/CanSum.java
+++ src/dynamic_programming/CanSum.java
@@ -8,25 +8,29 @@ public class CanSum {
     private static final Map<Integer, Boolean> memo = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.println(canSum(7, new int[]{5, 3, 4, 8}));
+        System.out.println(canSum(7, new int[]{5, 3, 4, 7}));
     }
 
     public static boolean canSum(int targetSum, int[] nums) {
-        if(memo.containsKey(targetSum)){
-            return memo.get(targetSum);
-        }
+//        if(memo.containsKey(targetSum)){
+//            return memo.get(targetSum);
+//        }
 
         if(targetSum == 0) {
             return true;
         }
+        if(targetSum < 0) return  false;
 
-        if(targetSum < 0)
-            return false;
+
 
         for (int num : nums) {
-            memo.put(targetSum,canSum(targetSum - num, nums));
+//            memo.put(targetSum,canSum(targetSum - num, nums));
+            if(canSum(targetSum - num, nums)){
+                return true;
+            }
         }
 
-        return  memo.get(targetSum);
+//        return  memo.get(targetSum);
+        return false;
     }
 }
