c9366c20be59c82c8530a6f5ba14ccca551100a0
Ayodeji Bankole <bankymono@gmail.com>
2025-03-25 07:51:59 +0100

    #

--- src/sliding_window/Problem45.java
+++ src/sliding_window/Problem45.java
@@ -1,7 +1,42 @@
 package sliding_window;
 
+import java.util.List;
+
 public class Problem45 {
     public static void main(String[] args) {
+        String s = "abciiidef";
+        int k = 3;
 
+        System.out.println(maxVowelSub(s, k));
+    }
+
+    public static int maxVowelSub(String s, int k){
+        List<Character> vowels = List.of('a','e','i','o','u');
+        int maxVowel ;
+        int windowVowelCount = 0;
+
+        for(int i = 0; i < k; i++) {
+            char currentChar = s.charAt(i);
+            if(vowels.contains(currentChar)){
+                windowVowelCount++;
+            }
+        }
+
+        maxVowel = windowVowelCount;
+
+        for(int i = k; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            char outChar = s.charAt(i - k);
+            if(vowels.contains(outChar)){
+                windowVowelCount--;
+            }
+            if(vowels.contains(currentChar)){
+                windowVowelCount++;
+            }
+
+            maxVowel = Math.max(maxVowel, windowVowelCount);
+        }
+
+        return maxVowel;
     }
 }
