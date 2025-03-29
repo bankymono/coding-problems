1ca37b6bf973be40510a4e289a36a03d3de1cc2e
Ayodeji Bankole <bankymono@gmail.com>
2025-03-29 09:30:56 +0100

    #

--- src/dynamic_programming/CountPath.java
+++ src/dynamic_programming/CountPath.java
@@ -1,18 +1,55 @@
 package dynamic_programming;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CountPath {
     public static void main(String[] args) {
+//        List<List<String>> grid = Arrays.asList(
+//                Arrays.asList("O", "O", "O", "O", "O"),
+//                Arrays.asList("O", "O", "O", "X", "O"),
+//                Arrays.asList("O", "O", "O", "O", "O"),
+//                Arrays.asList("O", "O", "O", "O", "O"),
+//                Arrays.asList("O", "O", "O", "O", "O")
+//        );
 
+        List<List<String>> grid = Arrays.asList(
+//                Arrays.asList("O", "O", "O", "O", "O"),
+//                Arrays.asList("O", "O", "O", "X", "O"),
+//                Arrays.asList("O", "O", "O", "O", "O"),
+                Arrays.asList("O", "X"),
+                Arrays.asList("O", "O")
+        );
+        System.out.println(countPath(grid));
     }
 
     public static int countPath(List<List<String>> grid) {
-        return countPath(0, 0, grid);
+        Map<String, Integer> memo = new HashMap<>();
+        return countPath(0, 0, grid, memo);
     }
 
-    public static int countPath(int r, int c, List<List<String>> grid){
-        if(r == grid.size() - 1 || c == grid.get(0).size());
-        return 45;
+    public static int countPath(int r, int c, List<List<String>> grid, Map<String, Integer> memo){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(r).append(",").append(c);
+
+        if(memo.containsKey(stringBuilder.toString())) return memo.get(stringBuilder.toString());
+
+        if(r == grid.size() || c == grid.get(0).size()) {
+            return 0;
+        }
+
+        if(grid.get(r).get(c).equals("X")) {
+            return  0;
+        }
+
+        if(r == grid.size() - 1 && c == grid.get(0).size() - 1){
+            return 1;
+        }
+
+        int result = countPath(r + 1, c, grid, memo) + countPath(r, c + 1, grid, memo);
+        memo.put(stringBuilder.toString(), result);
+        return result;
     }
 }
