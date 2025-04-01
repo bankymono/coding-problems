53b884d10373fd53c6c44e065e9e976cd894150a
Ayodeji Bankole <bankymono@gmail.com>
2025-04-01 18:13:54 +0100

    #

---
+++ src/dynamic_programming/CountingChange.java
@@ -0,0 +1,27 @@
+package dynamic_programming;
+
+public class CountingChange {
+    public static void main(String[] args) {
+        System.out.println(countingChange(4, new int[]{1, 2, 3}));
+        System.out.println(countingChange(5, new int[]{1, 2, 3, 4}));
+        System.out.println(countingChange(15, new int[]{7, 7, 7}));
+    }
+
+    public static int countingChange(int target, int[] coins) {
+        return countingChange(target, coins, 0);
+    }
+
+    public static int countingChange(int target, int[] coins, int index) {
+        if(target == 0) {
+            return 1;
+        }
+        if(target < 0 || index >= coins.length){
+            return 0;
+        }
+
+        int leftTree = countingChange(target - coins[index], coins, index);
+        int rightTree = countingChange(target, coins, index+1);
+
+        return leftTree + rightTree;
+    }
+}
---
+++ src/dynamic_programming/NonAdjacentSumArrays.java
@@ -0,0 +1,45 @@
+package dynamic_programming;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NonAdjacentSumArrays {
+    public static void main(String[] args) {
+        System.out.println(nonAdjacentArrays(List.of(2, 4, 5, 12)));
+    }
+
+    public static List<List<Integer>> nonAdjacentArrays(List<Integer> nums) {
+        return nonAdjacentArrays(nums, 0, new HashMap<>());
+    }
+
+    public static List<List<Integer>> nonAdjacentArrays(List<Integer> nums, int i, Map<Integer, List<List<Integer>>> memo) {
+//        if(memo.containsKey(i)) return memo.get(i);
+        if(i >= nums.size()){
+            List<List<Integer>> res = new ArrayList<>();
+            res.add(new ArrayList<>());
+            return res;
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<List<Integer>> leftTree = nonAdjacentArrays(nums, i + 2, memo);
+        for(List<Integer> arr : leftTree){
+            List<Integer> temp = new ArrayList<>();
+            temp.add(nums.get(i));
+            temp.addAll(arr);
+            result.add(temp);
+        }
+
+        List<List<Integer>> rightTree = nonAdjacentArrays(nums, i+1, memo);
+
+        result.addAll(rightTree);
+
+//        nums.get(i) + nonAdjacentArrays(nums, i+2, memo), nonAdjacentArrays(nums, i+1, memo));
+
+//        memo.put(i, result);
+        return result;
+    }
+
+}
