7c538419df2a2242c5a53936c9fcd1188af8edb5
Ayodeji Bankole <bankymono@gmail.com>
2025-04-05 09:32:18 +0100

    #

---
+++ src/dynamic_programming/Lc198.java
@@ -0,0 +1,38 @@
+package dynamic_programming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lc198 {
+    public static void main(String[] args) {
+        System.out.println(houseRobber(new int[]{1,2,3,1}));
+    }
+
+    public static int houseRobber(int[] nums) {
+        if(nums.length > 100 || nums.length < 1) {
+            return 0;
+        }
+        return rob(nums, 0, new HashMap<>());
+    }
+
+    public static int rob(int[] nums, int index, Map<Integer, Integer> memo) {
+        if(memo.containsKey(index)) {
+            memo.get(index);
+        }
+
+        if(index >= nums.length) {
+            return  0;
+        }
+
+        if(nums[index] > 400 || nums[index] < 0) return 0;
+
+        int leftTree = nums[index] + rob(nums, index + 2, memo);
+        int rightTree = rob(nums,  index + 1, memo);
+
+        int result = Math.max(leftTree, rightTree);
+
+        memo.put(index, result);
+
+        return result;
+    }
+}
