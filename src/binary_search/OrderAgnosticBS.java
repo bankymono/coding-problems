00aef8a19d104420e5d599659936b0191c3d251d
Ayodeji Bankole <bankymono@gmail.com>
2025-04-07 19:11:10 +0100

    #

--- src/binary_search/OrderAgnosticBS.java
+++ src/binary_search/OrderAgnosticBS.java
@@ -3,9 +3,9 @@ package binary_search;
 public class OrderAgnosticBS {
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0,2, 3,4,15, 16, 18, 22, 45, 89};
-        int[] arr2 = {89,45,22,18,16,15,4,3,0,-4,-12,-18};
-        int target = -18;
-        int ans = ogbs(arr2, target);
+        int[] arr2 = {89,45,22,18,16,15,4,3,2,0,-4,-12,-18};
+        int target = 22;
+        int ans = ogbs(arr, target);
         System.out.println(ans);
     }
 
