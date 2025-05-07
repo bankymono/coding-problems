970b3a4c0f5f571f14b77ee2c34b70561049648b
Ayodeji Bankole <bankymono@gmail.com>
2025-05-07 07:54:34 +0100

    #

---
+++ src/array_problems/Problem57.java
@@ -0,0 +1,35 @@
+package array_problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem57 {
+    public static void main(String[] args) {
+        String input = "abcabcbb";
+        System.out.println(longestSubstring(input));
+    }
+
+    public static int longestSubstring(String input) {
+        Map<Character, Integer> charMap = new HashMap<>();
+        int left = 0;
+        int maxLength = Integer.MIN_VALUE;
+
+        for(int right = 0; right < input.length(); right++) {
+            char currentChar = input.charAt(right);
+
+            if(charMap.containsKey(currentChar)) {
+                left = Math.max(left, charMap.get(currentChar) + 1);
+            }
+
+            charMap.put(currentChar, right);
+
+            maxLength = Math.max(maxLength, right - left + 1);
+
+
+        }
+
+
+
+        return maxLength;
+    }
+}
