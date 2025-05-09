75cca8a2cb696c82b8f8dc258a6d29502768a296
Ayodeji Bankole <bankymono@gmail.com>
2025-05-09 18:51:39 +0100

    #

---
+++ src/backtracking/Permutations.java
@@ -0,0 +1,37 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        System.out.println(permutations(nums));
+    }
+
+    public static List<List<Integer>> permutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtracking(result, new ArrayList<>(), nums);
+
+        return result;
+    }
+
+    public static void backtracking(List<List<Integer>> result, List<Integer> tempList, int[] nums){
+
+        if(tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+
+        for(int num : nums) {
+            if(tempList.contains(num)){
+                continue;
+            }
+            tempList.add(num);
+            backtracking(result, tempList, nums);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
