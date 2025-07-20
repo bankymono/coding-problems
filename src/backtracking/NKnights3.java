3e0581263ced336e7d441729ade43234ceea25db
Ayodeji Bankole <bankymono@gmail.com>
2025-07-20 20:42:48 +0100

    #

---
+++ src/backtracking/NKnights3.java
@@ -0,0 +1,83 @@
+package backtracking;
+
+public class NKnights3 {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        knight(board, 0, 0, n);
+    }
+
+    public static void knight(boolean[][] board, int row, int col, int knights){
+        if(knights == 0) {
+            display(board);
+            return;
+        }
+
+        if(row == board.length-1 && col == board[0].length) {
+            return;
+        }
+
+        if(col == board[0].length) {
+            knight(board, row + 1, 0, knights);
+            return;
+        }
+
+        if(isValid(board,row,col)) {
+            board[row][col] = true;
+            knight(board,row,col+1,knights - 1);
+            board[row][col] = false;
+        }
+
+        knight(board, row, col + 1, knights);
+    }
+
+    public static boolean isValid(boolean[][] board, int row, int col) {
+        if(validPosition(board,row - 1,col - 2)){
+            if(board[row - 1][col - 2]){
+                return false;
+            }
+        }
+
+        if(validPosition(board,row-1,col+2)){
+            if(board[row - 1][col + 2]){
+                return false;
+            }
+        }
+
+        if(validPosition(board,row-2,col-1)){
+            if(board[row - 2][col - 1]){
+                return false;
+            }
+        }
+
+        if(validPosition(board,row-2,col+1)){
+            if(board[row - 2][col + 1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean validPosition(boolean[][] board, int row, int col) {
+        if(row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void display(boolean[][] board) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length;j++) {
+                if(board[i][j]){
+                    System.out.print("K ");
+                }else{
+                    System.out.print("O ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
