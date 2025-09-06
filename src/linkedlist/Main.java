30291adcc2e7230d469eb7850e8bef921edb27ee
Ayodeji Bankole <bankymono@gmail.com>
2025-09-06 18:48:38 +0100

    #

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -18,18 +18,27 @@ public class Main {
 //        list.display();
 //        list.delete(1);
 //        list.display();
+//
+//
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insertFirst(5);
+//        list.insertFirst(7);
+//        list.insertLast(79);
+//        list.insert(5,12);
 
+//        list.display();
 
-        DLL list = new DLL();
-        list.insertFirst(3);
-        list.insertFirst(4);
-        list.insertFirst(5);
-        list.insertFirst(7);
-        list.insertLast(79);
-        list.insert(5,12);
-
+        CLL list  = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
         list.display();
 
+        list.delete(3);
+        list.display();
     }
 
 
