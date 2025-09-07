d9bd0b62d8cd0a7a624d126f18e991b5bec28cf5
Ayodeji Bankole <bankymono@gmail.com>
2025-09-07 19:46:26 +0100

    #

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -70,6 +70,22 @@ public class LinkedList {
         size++;
     }
 
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if(index == 0){
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        index = index - 1;
+        node.next = insertRec(val,index, node.next);
+        return node;
+    }
+
     public void insertV2(int val, int index) {
         if(index == 0){
             insertFirst(val);
--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -2,24 +2,23 @@ package linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-//        LinkedList list = new LinkedList();
-//
-//        list.insertFirst(3);
-//        list.insertFirst(4);
-//        list.insertFirst(5);
-//        list.insertFirst(7);
-//        list.display();
-//
-//        list.insertV2(1900, 2);
-//        list.display();
+        LinkedList list = new LinkedList();
+
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(7);
+        list.display();
+
+        list.insertV2(1900, 2);
+        list.display();
 //        list.deleteFirst();
 //        list.display();
 //        list.deleteLast();
 //        list.display();
-//        list.delete(1);
-//        list.display();
-//
-//
+        list.insertRec(8,3);
+        list.display();
+
 //        DLL list = new DLL();
 //        list.insertFirst(3);
 //        list.insertFirst(4);
@@ -30,15 +29,15 @@ public class Main {
 
 //        list.display();
 
-        CLL list  = new CLL();
-        list.insert(23);
-        list.insert(3);
-        list.insert(19);
-        list.insert(75);
-        list.display();
-
-        list.delete(3);
-        list.display();
+//        CLL list  = new CLL();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.display();
+//
+//        list.delete(3);
+//        list.display();
     }
 
 
